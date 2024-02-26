6677d080315fe7b53e9ddc188efd382907209074
azanahmed07 <160744720+azanahmed07@users.noreply.github.com>
2024-02-26 15:30:48 +0500

    Add files via upload
    First Assignment of 45 Exercise in Typescript at Governor Sindh IT Initiative

---
+++ aap.js
@@ -0,0 +1,2 @@
+var personName = "AZAN AHMED";
+console.log("hello ".concat(personName, " would you like to learn some python today?"));
---
+++ aap.ts
@@ -0,0 +1,2 @@
+let personName: string= "AZAN AHMED";
+console.log(`hello ${personName} would you like to learn some python today?`);
